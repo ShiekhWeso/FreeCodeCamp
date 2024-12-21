ab989a2579c49f884ab1c1eff916c6023ee85862
Weso <mohamed.hosam.eldeeen@gmail.com>
2024-12-21 19:32:03 +0200

    LuhnAlgorithm update 1.0

--- LuhnAlgorithm.py
+++ LuhnAlgorithm.py
@@ -1,6 +1,12 @@
+#1. From the right to left, double the value of every second digit; if the product is greater than 9, sum the digits of the products.
+#2. Take the sum of all the digits.
+#3. If the sum of all the digits is a multiple of 10, then the number is valid; else it is not valid.
+
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
-
+    card_number_reversed = card_number[-1:-5:-1]
+    print(card_number_reversed)
+    
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
