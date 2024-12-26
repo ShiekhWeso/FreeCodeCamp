b6d6de6d2c90c8a31be4128e1b1383a361fa74a4
Weso <mohamed.hosam.eldeeen@gmail.com>
2024-12-26 11:20:39 +0200

    Update LuhnAlgorithm.py

--- LuhnAlgorithm.py
+++ LuhnAlgorithm.py
@@ -4,15 +4,17 @@
 
 def verify_card_number(card_number):
     sum_of_odd_digits = 0
-    card_number_reversed = card_number[-1:-5:-1]
-    print(card_number_reversed)
+    card_number_reversed = card_number[::-1]
+    # print(card_number_reversed)
+    odd_digits = card_number_reversed[::2]
+    for i in odd_digits:
+        print(odd_digits)
     
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-
-    print(translated_card_number)
+    # print(translated_card_number)
 
     verify_card_number(translated_card_number)
 
