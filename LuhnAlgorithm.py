371038325041e81ea4107f6e2a8cebd311a5c8bf
Weso <mohamed.hosam.eldeeen@gmail.com>
2024-12-27 15:43:56 +0200

    LuhnAlgorithm latest update

--- LuhnAlgorithm.py
+++ LuhnAlgorithm.py
@@ -9,28 +9,29 @@ def verify_card_number(card_number):
     odd_digits = card_number_reversed[::2]
     
     for digit in odd_digits:
-        print(odd_digits)
         (sum_of_odd_digits) += int(digit)
-    print(sum_of_odd_digits)
+        
     sum_of_even_digits = 0
     even_digits = card_number_reversed[1::2]
+    
     for digit in even_digits:
-        print(digit)
         number = int(digit) *2
         if number >= 10:
             number = number // 10 + number % 10
-            print(number)
         sum_of_even_digits += number
     total = sum_of_even_digits + sum_of_odd_digits
     print(total)
+    
     return 0 == total % 10
     
 def main():
-    card_number = '4111-1111-4555-1142'
+    card_number = input(str("plz enter and your numbers"))
     card_translation = str.maketrans({'-': '', ' ': ''})
     translated_card_number = card_number.translate(card_translation)
-    # print(translated_card_number)
 
-    verify_card_number(translated_card_number)
+    if verify_card_number(translated_card_number):
+        print("VALID!")
+    else:
+        print("INVALID!")
 
 main()
