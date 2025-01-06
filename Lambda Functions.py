53a4f8f4f76903a6838b9e17f9d8bece7b001f79
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-01-06 13:14:36 +0200

    Lambda Functions update 1.3

--- Lambda Functions.py
+++ Lambda Functions.py
@@ -9,10 +9,6 @@ def total_expenses(expenses):
     return sum(map(lambda expense: expense['amount'],expenses))
 
 def filter_expenses_by_category(expenses, category):
-    pass
-    
-test = lambda x: x * 2
-print(list(map(test, [2, 3, 5, 8])))
-print(sum(map(test, [2, 3, 5, 8])))
+    lambda expense: expense['category'] == category
 
 expenses = []
