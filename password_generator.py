8a36cdbe425b7047aeb72241977a3336945bbb44
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-02-21 22:36:28 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -17,13 +17,16 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
         for _ in range(length):
             password += secrets.choice(all_characters)
         constraints = [
-            (nums,"")
+            (nums,"[0-9]"),
+            (lowercase, "[a-z]"),
+            (uppercase, "[A-Z]"),
+            (special_chars, "")
         ]
         
     return password
 
 # new_password = generate_password(8)
 # print(new_password)
-pattern = re.compile("i")
+pattern = '[a-z]t'
 quote = 'Not all those who wander are lost.'
-print(pattern.search(quote))
+print(re.findall(pattern, quote))  
