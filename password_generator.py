924ce8767c78c855b11709e1e0df071900777413
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-02-21 20:59:11 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,8 @@
+import re
 import secrets
 import string
 
-def generate_password(length):
+def generate_password(length, nums, special_chars, uppercase, lowercase):
     # Define the possible characters for the password
     letters = string.ascii_letters
     digits = string.digits
@@ -9,11 +10,20 @@ def generate_password(length):
 
     # Combine all characters
     all_characters = letters + digits + symbols
-    password = ""
-    # Generate password
-    for _ in range(length):
-        password += secrets.choice(all_characters)
+    
+    while True:    
+        password = ""
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+        constraints = [
+            (nums,"")
+        ]
+        
     return password
 
-new_password = generate_password(8)
-print(new_password)
+# new_password = generate_password(8)
+# print(new_password)
+pattern = re.compile("i")
+quote = 'Not all those who wander are lost.'
+print(pattern.search(quote))
