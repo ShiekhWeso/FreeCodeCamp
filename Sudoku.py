265fa16275fec5a7622f46f0166ab05c3ad94881
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-07-17 22:55:49 +0300

    Update Sudoku.py 1.1
    Fixing the output format.
    Fixing other bugs in the code.

--- Sudoku.py
+++ Sudoku.py
@@ -1,6 +1,15 @@
 class Board:
     def __init__(self, board):
         self.board = board
+        
+    def __str__(self):
+        board_str = ""
+        for row in self.board:
+            row_str = [str(i) if i else "*" for i in row]
+            board_str += " ".join(row_str)
+            board_str += "\n"
+        return board_str
+        
     def find_empty_cell(self):
         for row, contents in enumerate(self.board):
             try:    
@@ -21,7 +30,7 @@ class Board:
         col_start = (col // 3) * 3
         for row_no in range(row_start, row_start + 3):
             for col_no in range(col_start, col_start + 3):
-                if self.board[row_no][col_no]:
+                if self.board[row_no][col_no] == num:
                     return False
         return True
     
@@ -30,6 +39,7 @@ class Board:
         valid_in_row = self.valid_in_row(row, num)
         valid_in_col = self.valid_in_col(col, num)
         valid_in_square = self.valid_in_square(row, col ,num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
         
     def solver(self):
         if (next_empty := self.find_empty_cell()) is None:
@@ -37,7 +47,7 @@ class Board:
         for guess in range(1, 10):
             if self.is_valid(next_empty, guess):
                 row, col = next_empty
-                self.board[row][col]
+                self.board[row][col] = guess
                 if self.solver():
                     return True
                 self.board[row][col] = 0
@@ -46,7 +56,7 @@ class Board:
 def solve_sudoku(board):
     gameboard = Board(board)
     print(f"Puzzle to solve:\n{gameboard}")
-    if gameboard.solver() == True:
+    if gameboard.solver():
         print( f"Solved puzzle:\n{gameboard}")
     else:
         print("The provided puzzle is unsolvable.")
@@ -64,5 +74,4 @@ puzzle = [
   [1, 7, 0, 0, 0, 6, 0, 0, 5]
 ]
     
-gameboard = Board(puzzle)
-print(gameboard)
+solve_sudoku(puzzle)
