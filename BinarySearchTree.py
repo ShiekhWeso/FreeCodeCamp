d0ee163f5fee234dbdba4b60c556e191639abf7b
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-07-18 20:12:45 +0300

    Update BinarySearchTree.py

---
+++ BinarySearchTree.py
@@ -0,0 +1,12 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        
+    def _insert(self, node, key):
+        pass
