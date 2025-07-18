337f85db39277f87347289fc6d2c5db49d77cb5d
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-07-18 23:06:44 +0300

    Update BinarySearchTree 0.1

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -9,4 +9,24 @@ class BinarySearchTree:
         self.root = None
         
     def _insert(self, node, key):
-        pass
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
+    
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+        
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+    
+    def search(self, key):
+        return self._search(self.root, key)
+        
