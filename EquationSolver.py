a8cf7f232451fd093d3f6ff7843d123e8ff2ebcd
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-09-16 15:05:41 +0300

    QuadraticEquation
    -Introduced a QuadraticEquation class with solve and analyze methods, supporting quadratic equation solving and vertex analysis.
    -Improved equation string formatting to omit '1' coefficients before variables and standardized root return types for consistency.

--- EquationSolver.py
+++ EquationSolver.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import re
+import math
 
 class Equation(ABC):
     degree: int
@@ -27,9 +29,11 @@ class Equation(ABC):
                 terms.append(f"{coefficient:+}")
             elif n == 1:
                 terms.append(f"{coefficient:+}x")
+            elif n > 1:
+                terms.append(f"{coefficient:+}x^{n}")        
         equation_string = " ".join(terms) + " = 0"
         
-        return equation_string.strip("+")
+        return re.sub(r'(?<!\d)1(?=x)', '', equation_string.strip("+"))
     
     @abstractmethod
     def solve(self):
@@ -41,21 +45,52 @@ class Equation(ABC):
     
 class LinearEquation(Equation):
     degree = 1
-    
+
     def solve(self):
         a, b = self.coefficients.values()
         x = -b / a
-        return x
-    
+        return [x]  # Return the root in a list for consistency
+
     def analyze(self):
         slope, intercept = self.coefficients.values()
         return f"Slope: {slope}, Intercept: {intercept}"
+
+class QuadraticEquation(Equation):
+    degree = 2
     
-lin_eq = LinearEquation(2, 3)
+    def __init__(self, *args):
+        super().__init__(*args)
+        a, b, c = self.coefficients.values()
+        self.delta = b**2 - 4*a*c
+    
+    def solve(self):
+        if self.delta < 0:
+            return []
+        a, b, _ = self.coefficients.values()
+        x1 = (-b + (math.sqrt(self.delta))) / (2 * a)
+        x2 = (-b - (math.sqrt(self.delta))) / (2 * a)
+        
+        if self.delta == 0:
+            return [x1]
+        
+        return [x1, x2] 
+    
+    def analyze(self):
+        a, b, c = self.coefficients.values()
+        x = -b / (2 * a)
+        y = a * x**2 + b * x + c
+        
+        return {"x": x, "y": y}
+
+lin_eq = LinearEquation(1, 3)
 # LinearEquation(4, 5) ---> 4x + 5 = 0
 print(lin_eq)
 print(lin_eq.solve())
 print(lin_eq.analyze())
+
+quadr_eq = QuadraticEquation(1, 2, 1)
+print(quadr_eq)
+print(quadr_eq.solve())
 
 # Adding main function
 # making the code take input from the user
